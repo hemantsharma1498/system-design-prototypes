96b10171c185219ee885575fa4d20bcf596f38a4
Hemant Sharma <hemantsharma1498@gmail.com>
2024-06-30 21:04:16 +0530

    Finished connection balancer with unit tests

--- week 2/rtc/connection-balancer/server/handlers.go
+++ week 2/rtc/connection-balancer/server/handlers.go
@@ -2,15 +2,44 @@ package server
 
 import (
 	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
+	"strings"
 )
 
 
+type ServerAddressResponse struct {
+  Address string `json:"Address"`
+}
+
 
 func (c *ConnectionBalancer) GetCommServerAddress(w http.ResponseWriter, r *http.Request, db *sql.DB){
-  org := r.PathValue("org")
-  if len(org) == 0 {
-      
+//  org := r.PathValue("org")
+  orgArr := strings.Split(r.URL.Path, "/")
+  org := orgArr[len(orgArr)-1] 
+  if len(c.serverAddresses) == 0 {
+    c.LoadingStatus=false
+    c.LoadCommunicationServers(db)
+  }
+  var address string = ""
+  address = c.serverAddresses[org] 
+  if address == ""{
+    log.Printf("No address found for %s\n", org)
+    w.WriteHeader(http.StatusBadRequest)
+    w.Write([]byte("No address found for the given org name"))
+    return 
   }
   
+  resp := ServerAddressResponse{Address: address}
+  data, err := json.Marshal(resp)
+  if err != nil {
+    log.Printf("Encountered an error while marshalling address for %s\n", org)
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("Server encountered an error. Please try again."))
+    return
+  }
+  w.WriteHeader(http.StatusOK)
+  w.Write(data)
 }
+
--- week 2/rtc/connection-balancer/server/server.go
+++ week 2/rtc/connection-balancer/server/server.go
@@ -9,13 +9,14 @@ import (
 
 type ConnectionBalancer struct {
   Router *http.ServeMux
+  LoadingStatus bool
+  serverAddresses map[string]string
 }
 
-
-
 func InitServer(d *sql.DB) *ConnectionBalancer {
-  s := &ConnectionBalancer{ Router: http.NewServeMux(), }
-  s.LoadCommunicationServers(d)
+  s := &ConnectionBalancer{ Router: http.NewServeMux(), LoadingStatus: false, serverAddresses: make(map[string]string, 1)}
+  go s.LoadCommunicationServers(d)
+  s.Routes(d)
   return s
 }
 
@@ -29,5 +30,49 @@ func (c *ConnectionBalancer) Start(address string) error {
 
 
 func (c *ConnectionBalancer) LoadCommunicationServers(db *sql.DB){
-  
+  if err := c.getServerAddresses(db, 100, 0); err != nil {
+    log.Printf("Encountered an error while fetching server addresses: %s\n", err)
+  }
+  for {
+    if c.LoadingStatus==true {
+      log.Printf("Loaded addresses for %v servers\n", len(c.serverAddresses))
+      break
+    }
+  }
 }
+
+func (c *ConnectionBalancer) getServerAddresses(db *sql.DB, limit int, offset int) error {
+  rows, err := db.Query("SELECT org, address FROM communication_servers LIMIT ? OFFSET ?", limit, offset)
+  defer rows.Close()
+  if err != nil {
+    if err == sql.ErrNoRows {
+      c.LoadingStatus = true 
+      return nil
+    }
+    return err
+  }
+  foundRows := false
+  for rows.Next(){
+    foundRows = true
+    var (
+      org string
+      address string
+    )
+    if err := rows.Scan(&org, &address); err != nil {
+      return err
+    }
+    if c.serverAddresses[org] == "" {
+      c.serverAddresses[org]=address
+    }
+  }
+  
+  if !foundRows{
+    c.LoadingStatus = true 
+    return nil
+  }
+
+  c.getServerAddresses(db, limit+100, limit)
+
+  return nil
+}
+
