5f0f1e24c2c8174ebfbefca9e756b46f0b7b31fd
Hemant Sharma <hemantsharma1498@gmail.com>
2024-07-04 12:48:30 +0530

    More changes

--- week 2/rtc/members/go.mod
+++ week 2/rtc/members/go.mod
@@ -6,4 +6,10 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
+	google.golang.org/grpc v1.65.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 )
--- week 2/rtc/members/server/server.go
+++ week 2/rtc/members/server/server.go
@@ -2,25 +2,39 @@ package server
 
 import (
 	"log"
+	//"members/pkg/proto"
 	"members/store"
 	"net/http"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Members struct {
-	listenAddress string
-	Router        *http.ServeMux
-	store         store.Storage
+	listenAddressHttp string
+	Router            *http.ServeMux
+	GrpcClient        *grpc.ClientConn
+	store             store.Storage
 }
 
-func InitServer(listenAddress string, store store.Storage) *Members {
-	s := &Members{listenAddress: listenAddress, Router: http.NewServeMux(), store: store}
-    s.Routes()
+func InitServer(listenAddressHttp string, store store.Storage) *Members {
+	s := &Members{listenAddressHttp: listenAddressHttp, Router: http.NewServeMux(), store: store}
+	s.Routes()
 	return s
 }
 
 func (m *Members) Start() error {
-	log.Printf("Starting members server at address: %s\n", m.listenAddress)
-	if err := http.ListenAndServe(m.listenAddress, m.Router); err != nil {
+	log.Printf("Starting members server at address: %s\n", m.listenAddressHttp)
+
+	// Set up a connection to the server. @TODO CHANGE THE HARDCODED VALUE
+	m.GrpcClient, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	//c := proto.NewGreeterClient(conn)
+
+	if err := http.ListenAndServe(m.listenAddressHttp, m.Router); err != nil {
 		return err
 	}
 	return nil
