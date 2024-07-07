a38bad6db9127827a5343146e916d2e108f33f99
Hemant Sharma <hemantsharma1498@gmail.com>
2024-07-08 01:10:40 +0530

    Untested code for redis pub/sub

--- week 2/rtc/communications/cmd/communications/main.go
+++ week 2/rtc/communications/cmd/communications/main.go
@@ -1,7 +1,30 @@
 package main
 
-import ()
+import (
+	"communications/pkg/cache"
+	"communications/server"
+	"log"
+)
+
+const (
+	CacheAddr     = "localhost:6379"
+	CachePassword = ""
+)
+
+const httpAddress = ":3010"
+const grpcAddress = ":9091"
 
 func main() {
+	log.Printf("Initialising members server")
+
+	cache, err := cache.NewCache().Start(CacheAddr, CachePassword)
+	if err != nil {
+		log.Fatalf("Unable to start redis %s\n", err)
+	}
+
+	s := server.InitServer(cache)
+	if err = s.Start(httpAddress, grpcAddress); err != nil {
+		log.Panicf("Failed to initialise server at %s, error: %s\n", httpAddress, err)
+	}
 
 }
